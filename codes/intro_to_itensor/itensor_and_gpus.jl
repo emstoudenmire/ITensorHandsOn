0b3936994f04265c3aeda7a0c58c80072c47ff79
Karl Pierce <kmp5@vt.edu>
2024-08-13 12:46:26 -0400

    Update itensor + gpus

--- codes/intro_to_itensor/itensor_and_gpus.jl
+++ codes/intro_to_itensor/itensor_and_gpus.jl
@@ -1,5 +1,7 @@
 ### Depending on which GPU you have you can add different versions
 using Pkg
+active_dir = "$(@__DIR__)/.."
+Pkg.activate(active_dir)
 #Pkg.add("CUDA")
 Pkg.add("Metal")
 #Pkg.add("AMDGPU")
@@ -8,6 +10,7 @@ Pkg.add("Metal")
 ## ITensors alone has no specific GPU Array, it simply wraps
 ## existing GPU Libraries
 using ITensors
+using BenchmarkTools
 
 ## Next load the GPU library of choice and ITensors 
 ## associated GPU library will load
@@ -15,7 +18,8 @@ using ITensors
 using Metal
 #using AMDGPU
 
-dev = mtl # mtl cu rocm
+dev = mtl # mtl cu rocm NDTensors.cpu
+elt = (dev == mtl ? Float32 : Float64)
 ## construct some matrices
 a = randn(elt, 20, 30)
 b = randn(elt, 30, 40)
@@ -30,16 +34,28 @@ mul!(cc, ca, cb)
 
 Array(cc) ≈ c ## true
 
-using BenchmarkTools
-
 elt = Float32 ## Metal doesn't support F64
-I, J, K = (1000, 1000, 1000)
+I, J, K = (500, 500, 500)
 i,j,k = Index.((I, J, K))
 
 A = random_itensor(elt, i,j)
 B = random_itensor(elt, j,k)
 
 @btime A * B;
+mA, mB = dev.((A,B));
 @btime mA * mB;
 
 ## Test higher order tensor contractions
+
+L, M = 500, 500
+l, m = Index.((L, M))
+
+A = random_itensor(elt, i,k,j)
+B = random_itensor(elt, l,k)
+
+@btime A * B
+mA, mB = dev.((A,B))
+@btime mA * mB
+
+## Other libraries can do more efficient Tensor operations (such as TBLIS or cuTENSOR)
+## ITensor can interface these to make tensor network contractions more efficent.
