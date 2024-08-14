e0797d1aa4833134559e71e026f1a1e96b5fd531
Karl Pierce <kmp5@vt.edu>
2024-08-14 09:23:39 -0400

    Update tucker_decomposition code example

--- .development/completed_codes/tucker_decomposition.jl
+++ .development/completed_codes/tucker_decomposition.jl
@@ -2,10 +2,34 @@ using ITensors
 using LinearAlgebra
 ### Struct for the Tucker decomposition
 
-include("tucker_algorithms.jl")
 struct TuckerDecomp
   Core::ITensor
   Factors::Vector{ITensor}
+end
+
+abstract type TuckerAlgorithm end
+
+struct NaiveTucker <: TuckerAlgorithm
+end
+
+## Computing the SVD of each leg could be expensive
+## especially for many leg or large dimension legs.
+## It might be easier to compute a matrix multiplication of the tensor to make it square then compute the 
+## eigenvalue decomposition
+struct SmarterTucker <:TuckerAlgorithm
+end
+
+## Can you make an algorithm that truncates the tucker factors based off of the spectrum?
+struct TruncatedTucker 
+  cutoff::Number
+  solver::Union{TuckerAlgorithm, Nothing}
+end
+TruncatedTucker(cutoff::Number) = TruncatedTucker(cutoff, nothing)
+TruncatedTucker(cutoff::Number, solver::Type{<:TuckerAlgorithm}) = TruncatedTucker(cutoff, solver())
+
+## Can you use a randomized SVD algorithm instead of squaring the problem?
+struct RandomizedTucker <:TuckerAlgorithm
+  
 end
 
 TuckerDecomp(A::ITensor; algorithm=NaiveTucker()) = computeHOSVD(algorithm, A)
@@ -21,32 +45,14 @@ function computeHOSVD(::NaiveTucker, A::ITensor; cutoff=nothing)
   Factors = Vector{ITensor}([])
   for i in inds(A)
       #### For each leg one must compute the SVD 
-      
+      U, _, _ = svd(Core, (i,); cutoff=cutoff)
+      push!(Factors, U)
       ## Contract the factor with the partially transformed 
       ## Core tensor
-      
+      Core = U * Core
   end
   return TuckerDecomp(Core, Factors)
 end
-
-### Testing the naive tucker decomposition 
-elt = Float64
-a = rand(elt, 10,20);
-i,j = Index.((10,20));
-A = itensor(a, i,j);
-
-TD = TuckerDecomp(A);
-
-## The tucker decomposition for a matrix is the SVD so check the validity 
-A ≈ TD.Factors[1] * TD.Core * TD.Factors[2]
-
-i,j,k = Index.((100,200,300))
-D = itensor(randn(100,200,300), i,j,k)
-
-## This is a good reference for how much faster the smarter algorithm
-@time TD = TuckerDecomp(D);
-
-D ≈ contract(TD)
 
 function computeHOSVD(::SmarterTucker, A::ITensor; cutoff=nothing)
   Core = copy(A)
@@ -54,55 +60,25 @@ function computeHOSVD(::SmarterTucker, A::ITensor; cutoff=nothing)
 
   for i in inds(A)
       ## square the problem by priming the leg you want to decompose
-      
+      square = Core * prime(Core, i)
       #### The left singular vectors are now the eigenvectors of the problem 
-
+      _,U = eigen(square, i, i'; cutoff)
       
       U = noprime(real(U))
       push!(Factors, U)
       ## Contract the factor with the partially transformed 
       ## Core tensor
-      
+      Core = U * Core
   end
   return TuckerDecomp(Core, Factors)
 end
-
-function computeHOSVD(::RandomizedTucker, A::ITensor; cutoff=nothing)
-  Core = copy(A)
-  Factors = Vector{ITensor}([])
-
-  for i in inds(A)
-      ## compute the randomized projection of the target tensor
-      
-      #### Compute the SVD of this now smaller problem
-
-      
-      U = noprime(real(U))
-      push!(Factors, U)
-      ## Contract the factor with the partially transformed 
-      ## Core tensor
-      
-  end
-  return TuckerDecomp(Core, Factors)
-end
-
-algorithm = SmarterTucker()
-TD = TuckerDecomp(A; algorithm)
-
-A ≈ contract(TD)
-TD = @time TuckerDecomp(D; algorithm);
-D ≈ contract(TD)
 
 function computeHOSVD(T::TruncatedTucker, A::ITensor)
   solver = isnothing(T.solver) ? NaiveTucker() : T.solver
   computeHOSVD(solver, A; T.cutoff)
 end
 
-algorithm = TruncatedTucker(0.1, SmarterTucker)
-@time TD = TuckerDecomp(D; algorithm);
-## The decomposition is no longer exact, so fit is a good measure of the decomposition
 fit(Tucker, target) = 1.0 - norm(target - contract(Tucker))/ norm(target)
-
 
 ## You can easily use an alternating least squares algorithm to 
 ## systematically improve the Tucker factor matrices computed using the 
@@ -111,6 +87,31 @@ fit(Tucker, target) = 1.0 - norm(target - contract(Tucker))/ norm(target)
 ## Where U^{i} is the tucker factor associated with the ith mode in A.
 ## And C is the new optimized core tensor. C can also be expressed as Σ V^{k} derived from the SVD
 ## of the LHS of the equation.
+function HOOI(TD::TuckerDecomp, target::ITensor; niters = 1)
+  accuracy = 1.0 - norm(contract(TD) - target) / norm(target)
+  Factors = copy(TD.Factors)
+  Core = copy(TD.Core)
+  for iters in 1:niters
+      S = ITensor()
+      V = ITensor()
+      for i in 1:length(size(target))
+          list = Vector{ITensor}([target,])
+          append!(list, deleteat!(copy(Factors), [(i)]))
+          B = contract(list)
+          U,S,V = svd(B, ind(Factors[i],1); maxdim = dim(Factors[i],2))
+          Factors[i] = U
+      end
+      Core = S * V
+      curr_accuracy = 1.0 - norm(contract([Core, Factors...]) - target) / norm(target)
+      println("$iters \t $curr_accuracy")
+      if abs(accuracy - curr_accuracy) < 1e-10
+          return TuckerDecomp(Core, Factors)
+      end
+      accuracy = curr_accuracy
+  end
+  return TuckerDecomp(Core, Factors)
+end
+
 function HOOI(TD::TuckerDecomp, target::ITensor; niters = 1)
   accuracy = fit(TD, target)
   Factors = copy(TD.Factors)
@@ -121,13 +122,14 @@ function HOOI(TD::TuckerDecomp, target::ITensor; niters = 1)
       for i in 1:length(size(target))
           ## Make a list of tensors to contract. We want to contract
           ## the target tensor with all tucker factors except for the ith factor
-          contraction_list = Vector{ITensor}()
+          contraction_list =  Vector{ITensor}([target,])
+          append!(contraction_list, deleteat!(copy(Factors), [(i)]))
           
           B = contract(contraction_list)
 
           ## Compute the SVD of this new core tensor B with respect to the ith mode of target
           ## We also want to make sure that the rank of the SVD is equal to the current rank of Factors[i]
-          U,S,V = svd()
+          U,S,V = svd(B, ind(Factors[i],1); maxdim = dim(Factors[i],2))
           ## Update the ith factor
           Factors[i] = U
       end
@@ -144,7 +146,34 @@ function HOOI(TD::TuckerDecomp, target::ITensor; niters = 1)
   return TuckerDecomp(Core, Factors)
 end
 
-TD =  HOOI(TD, D; niters = 15);
+elt = Float64
+a = rand(elt, 10,20);
+i,j = Index.((10,20));
+A = itensor(a, i,j);
+
+TD = TuckerDecomp(A);
+
+## The tucker decomposition for a matrix is the SVD so check the validity 
+A ≈ TD.Factors[1] * TD.Core * TD.Factors[2]
+
+i,j,k = Index.((100,200,300))
+D = itensor(randn(100,200,300), i,j,k)
+@time TD = TuckerDecomp(D);
+
+D ≈ contract(TD)
+
+algorithm = SmarterTucker()
+TD = TuckerDecomp(A; algorithm)
+
+A ≈ contract(TD)
+TD = @time TuckerDecomp(D; algorithm);
+D ≈ contract(TD)
+
+algorithm = TruncatedTucker(0.1, SmarterTucker)
+@time TD = TuckerDecomp(D; algorithm);
+1.0 - norm(D - contract(TD))/ norm(D)
+
+@time TD =  HOOI(TD, D; niters = 15);
 
 ## This smaller example improves much more from HOOI.
 d = randn(Float64, 20,30,40)
