62dc8e173c5ab6ac6368911d436f911ef40845ed
Miles <miles.stoudenmire@gmail.com>
2025-07-17 07:50:15 -0400

    Pass in num_layers argument

--- codes/gate_evolution_tebd/tebd_gate_evolution.jl
+++ codes/gate_evolution_tebd/tebd_gate_evolution.jl
@@ -49,13 +49,12 @@ Keyword arguments:
 * maxdim - maximum allowed bond dimension of tensor network after each step
 * seed - random seed for making reproducible random gates
 """
-function run_tebd(; n=40, cutoff=default_cutoff(), maxdim=default_maxdim(), seed=1)
+function run_tebd(; n=40, cutoff=default_cutoff(), maxdim=default_maxdim(), num_layers=4, seed=1)
   Random.seed!(seed)
   sites = qubit_sites(n)
   psi0 = initial_state(sites)
-  gates = make_random_gates(sites)
+  gates = make_random_gates(sites; num_layers)
   psi = tebd_gate_evolution(gates, psi0; cutoff, maxdim)
-
   @printf("Norm of final state = %.14f\n",inner(psi,psi))
   @printf("Overlap with initial state = %.14f\n",inner(psi0,psi))
   return
